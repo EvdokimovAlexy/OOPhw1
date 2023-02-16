efe64d9a248ec2d8a54d0503f90d3fd9770ce633
Prometheus <poligraphich@gmail.com>
2023-02-16 12:14:22 +0300

    Product

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,19 @@
+package org.example;
+public class Product {
+    private final String name;
+    private final Double price;
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+    public String getName() {
+        return name;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    @Override
+    public String toString(){
+        return String.format("name = %s ; price = %2.2f ; " , name, price);
+    }
+}
